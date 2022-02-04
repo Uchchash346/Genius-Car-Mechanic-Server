a9deb3fbf392f26282d7dd9379fe2d3c7d6f09eb
Uchchash346 <mru416@gmail.com>
2022-02-04 20:38:51 +0600

    Insert Service data to the mongodb cloud database

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = 5000;
 
-const uri = `mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.4sdse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4sdse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        console.log("Connected to Database");
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body
+            console.log('Hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        })
+
     }
-    finally{
+    finally {
         // await client.close();
     }
 }
