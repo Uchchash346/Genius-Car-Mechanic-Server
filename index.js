927b482fb7bd91f77d03806647ad0cef367d10c0
Uchchash346 <mru416@gmail.com>
2022-02-04 21:44:42 +0600

    Display all services and explore delete api

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ async function run() {
             res.json(result)
         })
 
+        //DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
     }
     finally {
         // await client.close();
